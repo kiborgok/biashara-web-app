35f51fe54e4e4db46aca99816426479472d26bf3
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-26 13:04:47 +0300

    add loading indicator getUser() func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-// import logo from './logo.svg';
-/* This example requires Tailwind CSS v2.0+ */
-import React, { useEffect, useState } from "react";
+/* Requires Tailwind CSS v2.0+ */
+import React, { useContext, useEffect, useState } from "react";
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import app from "./api/firebaseConfig";
 
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
@@ -11,15 +13,53 @@ import "./App.css";
 import LoginPage from "./components/SigninPage";
 import SignupPage from "./components/SignupPage";
 import ProfilePage from "./components/ProfilePage";
-// import ItemDetails from "./components/ItemDetails";
+import Admin from "./components/admin/Admin";
+import { UserContext } from "./context/UserContect";
 
 function App() {
+  const auth = getAuth(app);
   const [showProfile, setShowProfile] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { user, setUser } = useContext(UserContext);
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  function getUser() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoading(false);
+        setUser(user);
+      } else {
+        setLoading(false);
+        setUser(null);
+      }
+    });
+  }
+
+  if (loading && !user)
+    return (
+      <div
+        style={{
+          backgroundColor: "smoke",
+          position: "absolute",
+          top: "0px",
+          bottom: "0px",
+          right: "0px",
+          left: "0px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Loading...
+      </div>
+    );
+
   return (
     <>
       <div className="min-h-full">
         <NavBar showProfile={showProfile} setShowProfile={setShowProfile} />
-        <Header />
         {showProfile ? (
           <ProfilePage
             showProfile={showProfile}
@@ -30,7 +70,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/signin" element={<LoginPage />} />
-          {/* <Route path="/itemDetails" element={<ItemDetails />} /> */}
+          <Route path="/admin/*" element={<Admin />} />
         </Routes>
       </div>
     </>
