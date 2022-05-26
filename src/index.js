bc77a8787475456607905c678d8b4b0a93391711
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-26 13:02:24 +0300

    add UserContextProvider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { UserContextProvider } from "./context/UserContect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <UserContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserContextProvider>
   </React.StrictMode>
 );
 
