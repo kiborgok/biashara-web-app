1d94f2aab22826c9cb72f1f10390a9400db3abbd
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-26 13:00:21 +0300

    add admin files

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import AdminNav from "./AdminNav";
+import BusinessesTable from './BusinessesPage';
+import CategoriesTable from './CategoriesPage';
 import UsersTable from './UsersTable';
 
 function Admin() {
     return (
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 p-2">
         <AdminNav />
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <Routes>
             <Route path="" element={<h1>Admin dashboard</h1>} />
             <Route path="users" element={<UsersTable />} />
-            <Route path="categories" element={<h1>Categories dashboard</h1>} />
-            <Route path="businesses" element={<h1>Businesses dashboard</h1>} />
+            <Route path="categories" element={<CategoriesTable />} />
+            <Route path="businesses" element={<BusinessesTable />} />
           </Routes>
         </div>
       </div>
