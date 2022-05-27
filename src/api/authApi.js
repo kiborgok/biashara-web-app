6e819b2908f9d26194e5e9a6b713b3eceda60c9a
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-27 05:48:53 +0300

    remove code

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -15,30 +15,10 @@ export async function signUp(email, password) {
       email,
       password
     );
-  // try {
-  //   const { user } = await createUserWithEmailAndPassword(
-  //     auth,
-  //     email,
-  //     password
-  //   );
-  //   return user;
-  // } catch (error) {
-  //   const errorCode = error.code;
-  //   console.log(errorCode);
-  //   const errorMessage = error.message;
-  //   console.log(errorMessage);
-  // }
+
 }
 
 export async function signIn(email, password) {
     return await signInWithEmailAndPassword(auth, email, password);
-  // try {
-  //   const { user } = await signInWithEmailAndPassword(auth, email, password);
-  //   console.log(user);
-  // } catch (error) {
-  //   const errorCode = error.code;
-  //   console.log(errorCode);
-  //   const errorMessage = error.message;
-  //   console.log(errorMessage);
-  // }
+
 }
