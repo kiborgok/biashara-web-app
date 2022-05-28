434c1177fdd327a61acc922923e1316833c9657c
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-28 14:06:19 +0300

    structure code

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -8,17 +8,10 @@ import app from "./firebaseConfig";
 
 const auth = getAuth(app);
 
-
 export async function signUp(email, password) {
-    return await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-
+  return await createUserWithEmailAndPassword(auth, email, password);
 }
 
 export async function signIn(email, password) {
-    return await signInWithEmailAndPassword(auth, email, password);
-
+  return await signInWithEmailAndPassword(auth, email, password);
 }
