80366db2741fcd9a9b1c00a4aaaeb0a0f8ff27ae
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-30 20:29:14 +0300

    remove firebase imports

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -1,16 +1,3 @@
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  // doc,
-  // getDoc,
-  getDocs
-} from "firebase/firestore/lite";
-
-import app from "./firebaseConfig";
-
-const db = getFirestore(app);
-
 const BASE_URL = "http://127.0.0.1:9292"
 
 export async function addCategory(category) {
@@ -36,26 +23,3 @@ export async function getCategories() {
     console.log(error)
   }
 }
-
-// export async function addCategory(name) {
-//   try {
-//     const { id } = await addDoc(collection(db, "biashara-categories"), {
-//       name,
-//     });
-//     // const category = { id, name: categoryName };
-//     // const categories = await getCategories()
-//     return id
-//     // dispatch({type: "ADD_CATEGORY", payload: category})
-//   } catch (error) {}
-// }
-
-// export async function getCategories() {
-//     const categoriesCollection = collection(db, "biashara-categories");
-//     const categories = await getDocs(categoriesCollection);
-//     const categoriesList = categories.docs.map((doc) => {
-//       const id = doc.id;
-//       const data = { ...doc.data(), id };
-//       return data;
-//     });
-//     return categoriesList;
-//   }
