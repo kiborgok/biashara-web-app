67fd5583d6df342f98141cd01e246689f3f4e1e9
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-30 20:27:13 +0300

    remove imports

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,15 +1,3 @@
-import {
-  getAuth,
-  // createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-
-import app from "./firebaseConfig";
-
-const auth = getAuth(app);
-
-const BASE_URL = "http://127.0.0.1:9292";
-
 export async function signUp(user) {
   try {
     const response = await fetch(BASE_URL + "/api/v1/signup", {
