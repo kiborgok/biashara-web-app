056a288ab9400429cce6f0817a14d8fdef15eff6
Alex Kiborgok <borgokalek14@gmail.com>
2022-07-03 10:10:06 +0300

    remove secret_string

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,13 +2,11 @@ import { useState, useEffect } from "react";
 import { useToken } from "./useToken";
 import jwt from 'jwt-decode';
 
-const SECRET_KEY = "DiN=*&6HBKJ-NBvgT@YHV_HV#@$%JGHVb08yB_gDd";
-
 const useUser = () => {
     const [token] = useToken();
 
     const getUser = token => {
-        return jwt(token, SECRET_KEY);
+        return jwt(token);
     }
 
     const [user, setUser] = useState(() => {
